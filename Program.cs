e5d0da6d7c4d196a9d948ba032cf480f2f139c9c
Andrii543 <andrii.rusyn15@gmail.com>
2024-12-20 14:26:28 +0100

    To-do list

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+
+namespace Home_Work_13._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = UTF8Encoding.UTF8;
+
+            List<ToDoItem> list = new List<ToDoItem>();
+
+            while (true)
+            {
+                Console.WriteLine("Оберіть операцію:\n");
+
+                Console.WriteLine("1 - Додати справу");
+                Console.WriteLine("2 - Вивести всі справи");
+                Console.WriteLine("3 - Позначити справу як виконану");
+                Console.WriteLine("4 - Видалити справу");
+                Console.WriteLine("5 - Вихід\n");
+
+                string operation = Console.ReadLine();
+
+                switch (operation)
+                {
+                    case "1":
+                        ToDoItem.AddItem(list);
+                        break;
+                    case "2":
+                        ToDoItem.DisplayItem(list);
+                        break;
+                    case "3":
+                        ToDoItem.MarkCompilted(list);
+                        break;
+                    case "4":
+                        ToDoItem.RemoveItem(list);
+                        break;
+                    case "5":
+                        return;
+                    default:
+                        Console.WriteLine("Вибрана не правельна операція");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ ToDoItem.cs
@@ -0,0 +1,85 @@
+﻿namespace Home_Work_13._1
+{
+    public class ToDoItem
+    {
+
+        public string Item { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+        public ToDoItem(string item)
+        {
+            Item = item;
+            IsCompleted = false;
+        }
+
+
+        public string ToString()
+        {
+            return Item;
+        }
+
+        public static void AddItem(List<ToDoItem> list)
+        {
+            Console.Write("Введіть назву справи:");
+            string inputTitle = Console.ReadLine();
+            list.Add(new ToDoItem(inputTitle));
+            Console.WriteLine("Справу додано!");
+        }
+
+        public static void RemoveItem(List<ToDoItem> list)
+        {
+            DisplayItem(list);
+            if (list.Count > 0)
+            {
+                return;
+            }
+            Console.WriteLine("Введіть номнер справи для видалення: ");
+            if (int.TryParse(Console.ReadLine(), out int input) && input >= 1 && input <= list.Count)
+            {
+                list.RemoveAt(input - 1);
+                Console.WriteLine("Cправу видалено!");
+            }
+            else
+            {
+                Console.WriteLine("Невірний вибір справи");
+            }
+        }
+
+        public static void DisplayItem(List<ToDoItem> list)
+        {
+            if (list.Count == 0)
+            {
+                Console.WriteLine("Список срав порожній");
+            }
+            else
+            {
+                Console.WriteLine("Cписок справ:");
+                for (int i = 0; i < list.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1}.{list[i].ToString()}");
+                }
+            }
+        }
+
+        public static void MarkCompilted(List<ToDoItem> list)
+        {
+            DisplayItem(list);
+            if (list.Count == 0)
+            {
+                return;
+            }
+
+            Console.WriteLine("Введіть номер справи яку хочете завершити");
+            if (int.TryParse(Console.ReadLine(), out int input) && input >= 1 && input <= list.Count)
+            {
+                list[input - 1].IsCompleted = true;
+                Console.WriteLine("Справу позначено як завершину!");
+            }
+            else
+            {
+                Console.WriteLine("Невірний вибір справи");
+            }
+        }
+    }
+}
